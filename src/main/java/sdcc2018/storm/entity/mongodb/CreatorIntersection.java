5a78ae476b20cb989fddc1075e4755fef0bc98fa
cristian <miliacristian4@gmail.com>
2018-10-07 17:53:08 +0200

    q

--- src/main/java/sdcc2018/storm/entity/mongodb/CreatorIntersection.java
+++ src/main/java/sdcc2018/storm/entity/mongodb/CreatorIntersection.java
@@ -1,8 +1,37 @@
 package sdcc2018.storm.entity.mongodb;
 
-public class CreatorIntersection {
-    public static void main(String args[]){
+import com.mongodb.MongoClient;
+import com.mongodb.MongoClientURI;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
+import org.bson.Document;
+import sdcc2018.storm.entity.Intersection;
 
+import java.io.InputStream;
+import java.util.Properties;
+
+public class CreatorIntersection {
+    private Properties properties;
+    public CreatorIntersection()throws Exception{
+        properties=new Properties();
+        InputStream is=this.getClass().getResourceAsStream("/config.properties");
+        properties.load(is);
+    }
+    public static void main(String args[])throws Exception{
+        CreatorIntersection creator=new CreatorIntersection();
+        MongoClientURI connectionString = new MongoClientURI(creator.properties.getProperty("urlMongoDB"));
+        MongoClient mongoClient = new MongoClient(connectionString);
+        MongoDatabase database = mongoClient.getDatabase(creator.properties.getProperty("mongoDBName"));
+        MongoCollection<Document> bookmarksCollection = database.getCollection("sdccIntersection");
+        Document document = new Document();
+
+        IntersectionGUI intersection=new IntersectionGUI();
+
+        String title ="miao";
+        document.append("name", title);
+        String tags="l";
+        document.append("tags", tags);
+        bookmarksCollection.insertOne(document);
         return;
     }
 }
--- src/main/java/sdcc2018/storm/entity/mongodb/IntersectionGUI.java
+++ src/main/java/sdcc2018/storm/entity/mongodb/IntersectionGUI.java
@@ -1,12 +1,33 @@
 package sdcc2018.storm.entity.mongodb;
 
-import sdcc2018.storm.entity.mongodb.CustomPhase;
-import sdcc2018.storm.entity.mongodb.CustomSensor;
-
 import java.util.List;
 
 public class IntersectionGUI {
     private List<CustomSensor> listSens;
     private int id;
     private List<CustomPhase>listPhase;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public List<CustomPhase> getListPhase() {
+        return listPhase;
+    }
+
+    public void setListPhase(List<CustomPhase> listPhase) {
+        this.listPhase = listPhase;
+    }
+
+    public List<CustomSensor> getListSens() {
+        return listSens;
+    }
+
+    public void setListSens(List<CustomSensor> listSens) {
+        this.listSens = listSens;
+    }
 }
