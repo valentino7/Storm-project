853b844b71b1234d59d374194dd49a016a74f940
cristian <miliacristian4@gmail.com>
2018-10-09 18:32:03 +0200

    q

--- src/main/java/sdcc2018/storm/stateTrafficLight/CustomMongoUpdateMapperQuery4.java
+++ src/main/java/sdcc2018/storm/stateTrafficLight/CustomMongoUpdateMapperQuery4.java
@@ -1,6 +1,5 @@
 package sdcc2018.storm.stateTrafficLight;
 
-import org.apache.kafka.common.serialization.StringSerializer;
 import org.apache.storm.mongodb.common.mapper.MongoMapper;
 import org.apache.storm.tuple.ITuple;
 import org.bson.Document;
@@ -26,29 +25,6 @@ public class CustomMongoUpdateMapperQuery4 implements MongoMapper {
         }
         document.append("stateTrafficLight",list);
         return new Document("$set", document);
-
-
-
-        /*Document document = new Document();
-        Sensor s = (Sensor) tuple.getValueByField(Costant.SENSOR);
-        List<Document> documentList=new ArrayList<Document>();
-
-        Document trafficLight = new Document();
-        trafficLight.append("0",s.getStateTrafficLight()[0]);
-        documentList.add(trafficLight);
-        trafficLight=null;
-
-        trafficLight=new Document();
-        trafficLight.append("1",s.getStateTrafficLight()[1]);
-        documentList.add(trafficLight);
-        trafficLight=null;
-
-        trafficLight=new Document();
-        trafficLight.append("2",s.getStateTrafficLight()[0]);
-        documentList.add(trafficLight);
-
-        document.append("stateTrafficLight", documentList);
-        return new Document("$set", document);*/
     }
 
     public CustomMongoUpdateMapperQuery4 withFields(String... fields) {
